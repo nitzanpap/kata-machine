988bad97fd3bd5650fdec986b3278ab4d6cd42dc
nitzanpap <nitzanpap@gmail.com>
2024-01-31 00:55:18 +0200

    feat: binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    console.log(haystack);
+    let minI = 0;
+    let maxI = haystack.length - 1;
+    while (minI <= maxI) {
+        const currentI = Math.floor((maxI - minI) / 2) + minI;
+        console.log(minI, currentI, maxI);
+        console.log(needle, haystack[currentI]);
 
+        if (needle === haystack[currentI]) {
+            return true;
+        }
+        if (needle < haystack[currentI]) {
+            maxI = currentI - 1;
+        } else {
+            minI = currentI + 1;
+        }
+    }
+    return false;
 }
